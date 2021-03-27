e193ac4823531d842cd00b4140ac8eb06d7c2c28
John Cable <johncable@talk21.com>
2021-03-27 11:15:53 +0000

    Start of Chapter 09

---
+++ Ruby/Chapter09/features/step_definitions/account_steps.rb
@@ -0,0 +1,9 @@
+Given(/^my account has been credited with \$(\d+)$/) do |amount|
+    my_account.credit(amount)
+end
+
+Then(/^the balance of my account should be \$(\d+)$/) do |amount|
+    sleep 1
+    expect(my_account.balance).to eq(amount),
+        "Expected the balance to be #{amount} but it was #{my_account.balance}"
+end
